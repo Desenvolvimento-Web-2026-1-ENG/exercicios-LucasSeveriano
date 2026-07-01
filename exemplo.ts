8ca682bb2a0f3341cb1a7eff2db4995f2473ee9d
Lucas_Severiano <148237063+Lucas12094@users.noreply.github.com>
2026-07-01 15:19:18 -0300

    Exemplo TypeScript

---
+++ exemplo.ts
@@ -0,0 +1,13 @@
+interface User {
+id: number;
+name: string;
+email: string;
+idade: number;
+}
+const usuariol: User = {
+id: 1,
+name: "João",
+email: "joao@example.com",
+idade: 30
+};
+console.log(usuariol);
